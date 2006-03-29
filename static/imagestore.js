a0fb3dd733ad55cbf6b209eda42132a9cdd22080
jeremy@goop.org <none@none>
2006-03-28 17:46:47 -0800

    Moved all the authentication stuff into a singleton object. Separated out the UI update stuff using dojo's connect() stuff.

--- static/imagestore.js
+++ static/imagestore.js
@@ -6,75 +6,6 @@ var logging = 0;
 
 var base_path = '/imagestore/';
 
-var authstate = {
-	state: 'unset',		// unset, set, valid, invalid
-	user: null,
-	pass: null,
-	response: {},
-	challenge: null,
-	challenge_expire: 0,
-	authcount: 1
-};
-
-function setstate(state)
-{
-	log('authstate.state '+authstate.state+' -> '+state);
-	authstate.state = state;
-}
-
-function set_userpass(user, pass)
-{
-	if (user == '')
-		user = null;
-	if (pass == '')
-		pass = null;
-
-	if (user == null || pass == null)
-		return logout();
-
-	if (user == authstate.user && pass == authstate.pass) {
-		log("setpass unchanged: "+authstate.pass+", "+authstate.pass);
-		return;
-	}
-
-	log("setpass changed: "+user+", "+pass);
-
-	setstate('set');
-
-	authstate.user = user;
-	authstate.pass = pass;
-	authstate.response = {};
-	//authstate.challenge = null;
-
-	window.user = user;
-	window.pass = pass;
-
-	update_auth();
-}
-
-function logout()
-{
-	setstate('unset');
-
-	authstate.user = null;
-	authstate.pass = null;
-
-	document.cookie = 'IS-authorization=;path='+base_path+';max-age=0;';
-
-	update_auth();
-}
-
-function log(str)
-{
-	if (!logging)
-		return;
-
-	l = dojo.byId("log");
-
-	if (l)
-		l.innerHTML += str + "<br>\n";
-}
-
 /*
   Authentication algorithm:
   - if we have a cached token for URL, then use it immediately,
@@ -89,150 +20,294 @@ function log(str)
 
   The challenge is common to all responses (it is not URI-specific),
   so it is cached to save on turnarounds.
+
+  Also, the server's authenticator sets a cookie, which is used in the
+  case where this code has no useful authentication information.
 */
 
-// This matches name=value, where value can either be a number, an identifier or a quoted string.
-var authitem = /\s*([a-z_][a-z0-9_-]*)\s*=\s*(\d+|[a-z_][a-z0-9_-]*|"([^"]*)"),?\s*(.*)$/i; // ";
 
-function parse_challenge(authhdr)
-{
-	if (authhdr.split(/\W+/)[0] != 'Digest')
-		alert('bad scheme: '+authhdr);
-	else
-		authhdr = authhdr.replace(/^Digest\s+/, '');
+var auth = {
+	state: 'unset',		// unset, set, valid, invalid
+	user: null,
+	pass: null,
+	response: {},
+	challenge: null,
+	challenge_expire: 0,
+	authcount: 1,
 
-	ret = {};
+	userid: null,		// set when state valid
+	fullname: null,		// set when state valid
 
-	//alert('authhdr="'+authhdr+'" len='+authhdr.length);
+	login: function(user, pass) {
+		if (user == '')
+			user = null;
+		if (pass == '')
+			pass = null;
 
-	while (authhdr.length > 0) {
-		var matches = authitem.exec(authhdr);
+		if (user == null || pass == null)
+			this.logout();
 
-		if (matches == null)
-			break;
-		
-		//alert('authitem='+authitem+' matches='+matches);
-
-		var name, value;
-
-		name = matches[1];
-		value = matches[2];
-		if (matches[3] != null)
-			value = matches[3];
-
-		authhdr = matches[4];
-
-		//alert('name='+name+' value='+value);
-		
-		ret[name] = value;
-	}
-
-	return ret;
-}
-
-// Given some details and a challenge, generate a digest authorization
-// header string
-function authenticate(user, pass, method, uri, challenge)
-{
-	function H(x) {
-		return dojo.crypto.MD5.compute(x, dojo.crypto.outputTypes.Hex);
-	}
-
-	function KD(secret, data) {
-		return H([ secret, data ].join(':'));
-	}
-
-	function A1(user, realm, password) {
-		return [ user, realm, password ].join(':');
-	}
-
-	function A2(method, uri) {
-		return [ method, uri ].join(':');
-	}
-
-	method = method.toUpperCase();
-
-	response = {
-		username: user,
-		realm: challenge.realm,
-		nonce: challenge.nonce,
-		uri: uri,
-
-		nc: authstate.authcount++,
-		cnonce: Math.floor(Math.random() * 1000000000).toString(),
-		qop: 'auth'
-	};
-
-	var pieces = [ response.nonce ];
-
-	if (challenge.qop)
-		pieces = pieces.concat(response.nc,
-				       response.cnonce,
-				       response.qop);
-
-	pieces = pieces.concat(H(A2(method, response.uri)));
-
-	response.response = KD(H(A1(user, response.realm, pass)),
-			       pieces.join(':'));
-
-	var ret = [];
-	
-	for (var k in response) {
-		var r = dojo.lang.reprString(response[k].toString());
-		//alert('k='+k+' -> '+r);
-		ret.push(k + '=' + r);
-	}
-
-	ret = ret.join(',');
-	
-	//alert(['user=', user, 'pass=', pass, 'method=', method, 'uri=', uri, 'challenge=', challenge].join(' '));
-
-	//alert('ret='+ret);
-
-	return 'Digest '+ret;
-}
-
-function get_challenge()
-{
-	var now = Math.floor((new Date()).getTime() / 1000); // milliseconds to seconds
-
-	if (authstate.challenge &&
-	    (authstate.challenge_expire == 0 || authstate.challenge_expire > now))
-		return authstate.challenge;
-
-	if (0)
-		alert('getting new challenge: now='+now+
-		      ' authstate.challenge='+ authstate.challenge+
-		      ' authstate.challenge_expire='+authstate.challenge_expire);
-	
-
-	var ret = null;
-
-	var req = {
-		url: base_path+'auth/challenge',
-		mimetype: 'text/json',
-		sync: true,
-		sendTransport: false,
-		preventCache: true,
-		load: function(type, data, event) {
-			ret = data;
-			//log('challenge: '+data);
-		},
-		error: function(type, data, event) {
-			alert('get challenge failed: '+event.error);
+		if (user == this.user && pass == this.pass) {
+			log("setpass unchanged: "+this.pass+", "+this.pass);
+			return;
 		}
-	};
-	dojo.io.bind(req);
 
-	if (ret) {
-		authstate.challenge_expire = ret.expire;
-		if (authstate.challenge_expire)
-			authstate.challenge_expire -= (60); // give 60 sec leeway
-		authstate.challenge = ret.challenge;
+		log("setpass changed: "+user+", "+pass);
+
+		this.user = user;
+		this.pass = pass;
+		this.response = {};
+
+		this._setstate('set');
+
+		this.update_auth();
+	},
+
+	logout: function() {
+		this.user = null;
+		this.pass = null;
+		this.response = {};
+
+		document.cookie = 'IS-authorization=;path='+base_path+';max-age=0;';
+
+		this._setstate('unset');
+
+		this.update_auth();
+	},
+
+	_setstate: function(state, fullname, userid) {
+		log('this.state '+this.state+' -> '+state+'('+fullname+','+userid+')');
+		this.state = state;
+		this.fullname = fullname;
+		this.userid = userid;
+	},
+
+	update_auth: function() {
+		var auth = this;
+
+		var req = {
+			url: base_path+'auth/user',
+			mimetype: 'text/json',
+
+			error: function(type, data, event) {
+				alert('user probe failed: '+event.status);
+			},
+			load: function(type, data, event) {
+				log('update_auth: user="'+data+'" auth.state='+auth.state);
+
+				if (data)
+					auth._setstate('valid', data.fullname, data.id);
+				else {
+					if (auth.state == 'set')
+						auth._setstate('invalid');
+					else
+						auth._setstate('unset');
+				}
+			}
+		};
+
+		log("updating auth");
+	
+		request(req);
+	},
+
+	_auth_response: function(method, uri, challenge) {
+		function H(x) {
+			return dojo.crypto.MD5.compute(x, dojo.crypto.outputTypes.Hex);
+		}
+
+		function KD(secret, data) {
+			return H([ secret, data ].join(':'));
+		}
+
+		function A1(user, realm, password) {
+			return [ user, realm, password ].join(':');
+		}
+
+		function A2(method, uri) {
+			return [ method, uri ].join(':');
+		}
+
+		method = method.toUpperCase();
+
+		var auth = this;
+
+		var response = {
+			username: auth.user,
+			realm: challenge.realm,
+			nonce: challenge.nonce,
+			uri: uri,
+
+			nc: auth.authcount++,
+			cnonce: Math.floor(Math.random() * 1000000000).toString(),
+			qop: 'auth'
+		};
+
+		var pieces = [ response.nonce ];
+
+		if (challenge.qop)
+			pieces = pieces.concat(response.nc,
+					       response.cnonce,
+					       response.qop);
+
+		pieces = pieces.concat(H(A2(method, response.uri)));
+
+		response.response = KD(H(A1(this.user, response.realm, this.pass)),
+				       pieces.join(':'));
+
+		var ret = [];
+	
+		for (var k in response) {
+			var r = dojo.lang.reprString(response[k].toString());
+			//alert('k='+k+' -> '+r);
+			ret.push(k + '=' + r);
+		}
+
+		ret = ret.join(',');
+	
+		//alert(['user=', user, 'pass=', pass, 'method=', method, 'uri=', uri, 'challenge=', challenge].join(' '));
+
+		//alert('ret='+ret);
+
+		return 'Digest '+ret;
+	},
+
+	_parse_challenge: function(authhdr) {
+		var authitem = /\s*([a-z_][a-z0-9_-]*)\s*=\s*(\d+|[a-z_][a-z0-9_-]*|"([^"]*)"),?\s*(.*)$/i; // )";
+
+		// This matches name=value, where value can either be a number, an identifier or a quoted string.
+	
+		if (authhdr.split(/\W+/)[0] != 'Digest')
+			alert('bad scheme: '+authhdr);
+		else
+			authhdr = authhdr.replace(/^Digest\s+/, '');
+
+		ret = {};
+
+		//alert('authhdr="'+authhdr+'" len='+authhdr.length);
+
+		while (authhdr.length > 0) {
+			var matches = authitem.exec(authhdr);
+
+			if (matches == null)
+				break;
+		
+			//alert('authitem='+authitem+' matches='+matches);
+
+			var name, value;
+
+			name = matches[1];
+			value = matches[2];
+			if (matches[3] != null)
+				value = matches[3];
+
+			authhdr = matches[4];
+
+			//alert('name='+name+' value='+value);
+		
+			ret[name] = value;
+		}
+		
+		return ret;
+	},
+
+	_get_challenge: function() {
+		var now = Math.floor((new Date()).getTime() / 1000); // milliseconds to seconds
+
+		if (this.challenge &&
+		    (this.challenge_expire == 0 || this.challenge_expire > now))
+			return this.challenge;
+
+		if (0)
+			alert('getting new challenge: now='+now+
+			      ' this.challenge='+ this.challenge+
+			      ' this.challenge_expire='+this.challenge_expire);
+	
+
+		var ret = null;
+
+		var req = {
+			url: base_path+'auth/challenge',
+			mimetype: 'text/json',
+			sync: true,
+			sendTransport: false,
+			preventCache: true,
+			load: function(type, data, event) {
+				ret = data;
+				//log('challenge: '+data);
+			},
+			error: function(type, data, event) {
+				alert('get challenge failed: '+event.error);
+			}
+		};
+		dojo.io.bind(req);
+
+		if (ret) {
+			this.challenge_expire = ret.expire;
+			if (this.challenge_expire)
+				this.challenge_expire -= (60); // give 60 sec leeway
+			this.challenge = ret.challenge;
+		}
+
+		return this.challenge;
+	},
+
+	authorize_request: function(origreq) {
+		var req = dojo.lang.shallowCopy(origreq);
+
+		if (this.state != 'unset') {
+			if (!this.response[req.url]) {
+				challenge = this._get_challenge();
+				//alert('got challenge for '+origreq.url+': '+challenge);
+			}
+
+			// If we have a challenge, then generate a response
+			if (challenge) {
+				var method = req.method;
+				if (!method)
+					method = 'get';
+				this.response[req.url] =
+					this._auth_response(method.toUpperCase(), req.url,
+							    this._parse_challenge(challenge));
+			}
+
+			// If we have a response, then be prepared to use it
+			if (this.response[req.url]) {
+				var resp = this.response[req.url];
+
+				//alert('url='+req.url+' auth='+auth);
+				if (req['headers'] == null)
+					req['headers'] = { };
+				req['headers']['authorization'] = resp;
+				// Opera, at least, seems to eat JS-created Authorization headers
+				req['headers']['x-authorization'] = resp;
+			} else {
+				// If we have no response, then be prepared to generate one.
+				req.error = function (type, data, event) {
+					if (event.status == 401) { // Unauthorized
+						this.challenge = event.getResponseHeader('www-authenticate');
+						dojo.io.bind(this.authorize_request(origreq));
+					} else
+						return origreq.error(type, data, event);
+				}
+			}
+		}
+
+		return req;
 	}
+};
 
-	return authstate.challenge;
+function log(str)
+{
+	if (!logging)
+		return;
+
+	l = dojo.byId("log");
+
+	if (l)
+		l.innerHTML += str + "<br>\n";
 }
+
 
 // Request a URL.  If we have been given a username and password, then
 // prepare to answer an authentication response.  Also keeps a cache
@@ -240,48 +315,7 @@ function get_challenge()
 // supply the response without being asked.
 function request(origreq, challenge)
 {
-	var req = dojo.lang.shallowCopy(origreq);
-
-	if (authstate.state != 'unset') {
-		// If we have no response, pre-get a challenge so we
-		// can avoid a 401 error
-		if (!challenge && !authstate.response[req.url]) {
-			challenge = get_challenge();
-			//alert('got challenge for '+origreq.url+': '+challenge);
-		}
-
-		// If we have a challenge, then generate a response
-		if (challenge) {
-			var method = req.method;
-			if (!method)
-				method = 'get';
-			var auth = authenticate(authstate.user, authstate.pass,
-						method.toUpperCase(),
-						req.url, parse_challenge(challenge));
-			authstate.response[req.url] = auth;
-		}
-
-		// If we have a response, then be prepared to use it
-		if (authstate.response[req.url]) {
-			var auth = authstate.response[req.url];
-
-			//alert('url='+req.url+' auth='+auth);
-			if (req['headers'] == null)
-				req['headers'] = { };
-			req['headers']['authorization'] = auth;
-			// Opera, at least, seems to eat JS-created Authorization headers
-			req['headers']['x-authorization'] = auth;
-		} else {
-			// If we have no response, then be prepared to generate one.
-			req.error = function (type, data, event) {
-				if (event.status == 401) { // Unauthorized
-					var challenge = event.getResponseHeader('www-authenticate');
-					request(origreq, challenge);
-				} else
-					return origreq.error(type, data, event);
-			}
-		}
-	}
+	var req = window.auth.authorize_request(origreq);
 
 	req.sendTransport = false;
 	req.preventCache = true;
@@ -289,67 +323,58 @@ function request(origreq, challenge)
 	dojo.io.bind(req);
 }
 
-function update_auth() {
+// Hook function to update the UI as the authentication state changes
+function _auth_state_updates ()
+{
 	var progress = dojo.byId("login.progress");
 	var login = dojo.byId('login');
 	var loginuser = dojo.byId('login.user');
 	var loginpass = dojo.byId('login.pass');
-	loginuser.value = authstate.user;
-	if (authstate.user == null)
-		loginuser.value = '';
-	
-	loginpass.value = authstate.pass;
-	if (authstate.pass == null)
-		loginpass.value = '';
+	var form = dojo.byId('login.form');
+	var state = dojo.byId('login.state');
+	var loginid = dojo.byId('login.loginid');
 
-	var req = {
-		url: base_path+'auth/user',
-		mimetype: 'text/json',
+	var auth = window.auth;
 
-		error: function(type, data, event) {
-			alert('user probe failed: '+event.status);
-		},
-		load: function(type, data, event) {
-			var form = dojo.byId('login.form');
-			var state = dojo.byId('login.state');
-			var loginid = dojo.byId('login.loginid');
+	function start_progress() {
+		progress.style.display = 'inline';
 
-			progress.style.display = 'none';
-			log('update_auth: user="'+data+'" auth.state='+authstate.state);
-
-			if (data) {
-				setstate('valid');
-				
-				log('data.username='+data.username+' fullname='+data.fullname);
-
-				loginid.innerHTML = data.fullname;
-				form.style.display = 'none';
-				state.style.display = 'inline';
-			} else {
-				if (authstate.state == 'set')
-					setstate('invalid');
-				else
-					setstate('unset');
-
-				loginid.innerHTML = 'Not logged in';
-				form.style.display = 'block';
-				state.style.display = 'none';
-			}
-			login.className = 'auth-' + authstate.state;
+		loginuser.value = (auth.user == null) && '' || auth.user;
+		loginpass.value = (auth.pass == null) && '' || auth.pass;
+	}
+	function update_form() {
+		progress.style.display = 'none';
+		if (auth.state == 'valid') {
+			loginid.innerHTML = auth.fullname;
+			form.style.display = 'none';
+			state.style.display = 'inline';
+		} else {
+			loginid.innerHTML = '';
+			form.style.display = 'block';
+			state.style.display = 'none';
 		}
-	};
+		login.className = 'auth-' + auth.state;
+	}
 
-	log("updating auth");
-	
-	login.className = 'auth-' + authstate.state;
-	progress.setAttribute('style', 'display:inline');
+	dojo.event.kwConnect({
+		  adviceType: 'before',
+				srcObj: auth,
+				srcFunc: 'update_auth',
+				adviceFunc: start_progress
+				});
+	dojo.event.kwConnect({
+		  adviceType: 'after',
+				srcObj: auth,
+				srcFunc: '_setstate',
+				adviceFunc: update_form
+				});
 
-	request(req);
+	auth.update_auth();
 }
+dojo.addOnLoad(_auth_state_updates);
 
-dojo.addOnLoad(update_auth);
-
-function setup_events()
+// Hook to cause authentication events as the UI state changes
+function _auth_ui_events()
 {
 	var login_submit = dojo.byId('login.submit');
 	var login_user = dojo.byId('login.user');
@@ -361,13 +386,13 @@ function setup_events()
 		var user = login_user.value;
 		var pass = login_pass.value;
 		
-		set_userpass(user, pass);
+		window.auth.login(user, pass);
 	}
 	dojo.event.connect(login_submit, 'onclick', submit_login);
 	dojo.event.connect(login_pass, 'onblur', submit_login);
 }
 
-dojo.addOnLoad(setup_events);
+dojo.addOnLoad(_auth_ui_events);
 
 poke = {
 	url: base_path+'default/1/meta/id',
